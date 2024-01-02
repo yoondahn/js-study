d3203eb3325a8fc834d43716c0b1fc5284dcce26
YoonDaHyun <101653672+yoondahn@users.noreply.github.com>
2024-01-02 11:57:11 +0900

    Add files via upload

---
+++ Vanilla JS/challenge1.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+const title = document.querySelector("h2");
+
+const superEventHandler = {
+  handleMouseEnter: function () {
+    title.innerHTML = "The mouse is here!";
+    title.style.color = colors[0];
+  },
+  handleMouseLeave: function () {
+    title.innerHTML = "The mouse is gone!";
+    title.style.color = colors[1];
+  },
+  handleWindowResize: function () {
+    title.innerHTML = "You just resized!";
+    title.style.color = colors[2];
+  },
+  handleContextMenu: function () {
+    title.innerHTML = "That was a right click!";
+    title.style.color = colors[3];
+  },
+};
+
+title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
+title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
+window.addEventListener("resize", superEventHandler.handleWindowResize);
+window.addEventListener("contextmenu", superEventHandler.handleContextMenu);
