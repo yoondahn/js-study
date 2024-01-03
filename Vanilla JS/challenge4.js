e5848a425793932ae4aab6f6c5ddc9d3f905631f
YoonDaHyun <101653672+yoondahn@users.noreply.github.com>
2024-01-03 22:59:05 +0900

    Add files via upload

---
+++ Vanilla JS/challenge4.js
@@ -0,0 +1,22 @@
+const clock = document.querySelector("h2#clock");
+
+const SECOND = 1000;
+const MINUTE = 1000*60;
+const HOUR = 1000*60*60;
+const DAY = 1000*60*60*24;
+
+function getDate() {
+    const eveDay = new Date("2024-12-24:00:00:00+0900");
+    const nowDay = new Date();
+    const timeGap = eveDay - nowDay;
+
+    const days = String(Math.floor(timeGap / DAY)).padStart(3, "0");
+    const hours = String(Math.floor((timeGap % DAY) / HOUR)).padStart(2, "0");
+    const minutes = String(Math.floor((timeGap % HOUR) / MINUTE)).padStart(2, "0");
+    const seconds = String(Math.floor((timeGap % MINUTE) / SECOND)).padStart(2, "0");
+    clock.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+
+getDate(); // 1초 먼저 호출
+setInterval(getDate, 1000);
+// setTimeout(getClock, 1000);
