f45a7cd68e4c9fd0657d3a5a6732f84ad7741662
YoonDaHyun <101653672+yoondahn@users.noreply.github.com>
2024-01-03 20:35:58 +0900

    Add files via upload

--- Vanilla JS/challenge2.js
+++ Vanilla JS/challenge2.js
@@ -8,13 +8,16 @@ function handleResize() {
   const width = window.innerWidth;
   if (width > 1000) {
     body.classList.add(BIG_SCREEN);
-    body.classList.remove(MEDIUM_SCREEN);
+    body.classList.remove(MEDIUM_SCREEN, SMALL_SCREEN);
+    console.log('big screen');
   } else if (width <= 1140 && width >= 700) {
     body.classList.add(MEDIUM_SCREEN);
     body.classList.remove(BIG_SCREEN, SMALL_SCREEN);
+    console.log('midium screen');
   } else {
-    body.classList.remove(MEDIUM_SCREEN);
+    body.classList.remove(MEDIUM_SCREEN, BIG_SCREEN);
     body.classList.add(SMALL_SCREEN);
+    console.log('small screen');
   }
 }
 
---
+++ Vanilla JS/study3.js
@@ -0,0 +1,43 @@
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+// const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    // loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    paintGreetings(username);
+    
+    if (username === "") {
+        alert("Please write your name");
+    } else if (username.length > 15) {
+        alert("Your name is too long.");
+    } else {
+        console.log(username);
+    }
+}
+
+function paintGreetings(username) {
+    // greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;   // backtick
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+// loginButton.addEventListener("click", onLoginBtnClick);
+// loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
