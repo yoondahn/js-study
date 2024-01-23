94397d9e531f10b24fadce973bbb06efd29facff
YoonDaHyun <101653672+yoondahn@users.noreply.github.com>
2024-01-23 22:29:02 +0900

    Add files via upload

--- React JS/NodeJS/App.js
+++ React JS/NodeJS/App.js
@@ -3,12 +3,43 @@ import './App.css';
 import Button from './Button';
 import {useState, useEffect} from "react";
 
+function Hello() {
+  // useEffect(function() {
+  //   console.log("hi");
+  //   return function() {
+  //     console.log("bye");
+  //   };
+  // }, [])
+
+  useEffect(() => {
+    console.log("hi");
+    return () => console.log("bye");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
   const [counter, setCounter] = useState(0);
   const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
 
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  // useEffect(() => {
+  //   console.log("I run when keyword&counter changes.");
+  // }, [keyword, counter]);
+
+  const [showing, setShowing] = useState(false);
+  const showClick = () => setShowing((prev) => !prev);
+  
   return (
     <div className="App">
       <header>
@@ -21,6 +52,9 @@ function App() {
             ></input>
             <h1>{counter}</h1>
             <button onClick={onClick}>click me</button>
+
+            {showing ? <Hello /> : null}
+            <button onClick={showClick}>{showing? "Hide" : "Show"}</button>
         </div>
       </header>
 
