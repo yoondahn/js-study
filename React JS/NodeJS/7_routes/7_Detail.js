1144221bc8339eea8560ec7e499807af05d9ddd3
YoonDaHyun <101653672+yoondahn@users.noreply.github.com>
2024-01-30 21:25:02 +0900

    Add files via upload

--- React JS/NodeJS/7_routes/7_Detail.js
+++ React JS/NodeJS/7_routes/7_Detail.js
@@ -1,5 +1,5 @@
-function Page() {
-    return <h1>Detail</h1>
+function Detail() {
+    return <h1>Detail</h1>;
 }
 
-export default Page();
+export default Detail;
--- React JS/NodeJS/7_routes/7_Home.js
+++ React JS/NodeJS/7_routes/7_Home.js
@@ -1,24 +1,12 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Movie from "../7_components/7_Movie";
-import "./7_Home.css";
 
 function Home() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
-    // useEffect(() => {
-    //     fetch(
-    //         `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    //     )
-    //     .then((response) => response.json())
-    //     .then((json) => {
-    //         setMovies(json.data.movies)
-    //         setLoading(false)
-    //     });
-    // }, []);
-    // console.log(movies);
 
-    const getMovies = async() => {
-        const json = await(
+    const getMovies = async () => {
+        const json = await (
             await fetch(
                 `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
             )
@@ -26,25 +14,27 @@ function Home() {
         setMovies(json.data.movies);
         setLoading(false);
     };
+
     useEffect(() => {
         getMovies();
     }, []);
-    console.log(movies);
 
     return (
-        <div className="movie-app">
+        <div>
           {loading ? (
             <h3>Loading...</h3>
+          ) : movies.length === 0 ? (
+            <h3>No movies found.</h3>
           ) : (
             <div>
               {movies.map((movie) => (
-                <Movie>
+                <Movie
                     key={movie.id}
                     coverImg={movie.medium_cover_image}
                     title={movie.title}
                     summary={movie.summary}
                     genres={movie.genres}
-                </Movie>
+                />
               ))}
             </div>
           )}
