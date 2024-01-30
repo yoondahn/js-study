599a373fdf9aac51b2b4b7c3d8194839dc0664d8
YoonDaHyun <101653672+yoondahn@users.noreply.github.com>
2024-01-30 21:23:57 +0900

    Add files via upload

--- React JS/NodeJS/7_MovieApp.js
+++ React JS/NodeJS/7_MovieApp.js
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./7_routes/7_Home";
+import Detail from "./7_routes/7_Detail";
+import ToDo from './7_ToDo';
+
 function MovieApp() {
-    return null;
+    return (
+      <Router>
+        <Switch>
+            <Route path="/movie">
+                <Detail />
+            </Route>
+            <Route path="/">
+                <ToDo />
+                <Home />
+            </Route>
+        </Switch>
+      </Router>  
+    );
 } 
 
 export default MovieApp;
--- React JS/NodeJS/index.js
+++ React JS/NodeJS/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 // import Effects from './6_Effects';
-import ToDo from './7_ToDo';
+// import ToDo from './7_ToDo';
 import MovieApp from './7_MovieApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ToDo />
+    {/* <ToDo /> */}
     <MovieApp />
     {/* <Effects /> */}
     {/* <App /> */}
