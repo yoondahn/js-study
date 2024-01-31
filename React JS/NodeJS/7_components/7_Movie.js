beb6a671a57c25ed73daa1c5e50d0fb44f7db8f5
YoonDaHyun <101653672+yoondahn@users.noreply.github.com>
2024-01-31 16:09:58 +0900

    Add files via upload

--- React JS/NodeJS/7_components/7_Movie.js
+++ React JS/NodeJS/7_components/7_Movie.js
@@ -1,26 +1,36 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import "../7_routes/7_Home.css";
  
-function Movie({ coverImg, title, summary, genres }) {
-    return(
-      <div>
+function Movie({ id, coverImg, title, year, summary, genres }) {
+  if (!summary) {
+    return null;
+  }
+  
+  return(
+    <div className="movie-app">
+      <div className="movie-card">
         <img src={coverImg} alt={title}/>
-        <div>
-          <h2><Link to="/movie">{title}</Link></h2>
-          <p>{summary}</p>
-          <ul>
+        <div className="movie-details">
+          <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
+          <h4>{year}</h4>
+          <p>{summary.length > 1500 ? `${summary.slice(0, 1500)}..` : summary}</p>
+          <ul className="movie-genres">
             {genres.map((g) => (
               <li key={g}>{g}</li>
             ))}
           </ul>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
-Movie.prototype = {
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
     coverImg: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
     summary: PropTypes.string.isRequired,
     genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
